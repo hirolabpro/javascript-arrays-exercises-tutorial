8c65f03d298719385f04947fc44d7df2493294c9
Daniel Ipince <ipince22@gmail.com>
2021-02-12 16:21:14 +0000

    feb-12-2021

--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -1,6 +1,37 @@
+
 let parking_state = [
-  [1,1,1],
+  [2,1,2],
   [0,0,0],
   [1,1,2]
 ]
+//console.log(getParkingLotState(parking_state))
+
+
+
+    function getParkingLotState(param){
+      let state = {
+        totalSlots: 0,
+        availableSlots: 0,
+        occupiedSlots: 0
+      }
+      for (let i = 0; i < param.length; i++){
+          //console.log(param[i]);
+        for (let x = 0; x < param[i].length; x++){
+            //console.log(param[i][x]);
+          if (param[i][x] === 2){
+            
+            state.availableSlots += 1
+            state.totalSlots += 1
+          }
+          else if (param[i][x] === 1){
+            state.occupiedSlots += 1
+            state.totalSlots += 1
+          }
+        }
+      }
+    return state
+    } 
+    
 console.log(getParkingLotState(parking_state))
+    
+
